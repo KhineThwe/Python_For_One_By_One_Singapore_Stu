f28329b697edfa8e290411402030dcedcfdacd4a
Khine <khinezar70122107@gmail.com>
2023-04-07 18:19:58 -1200

    set comprehension

--- Step2/3.DictComprehension.py
+++ Step2/3.DictComprehension.py
@@ -9,3 +9,7 @@ my_new_list = [1,2,3,4,5]
 new_dictionary1 = {num:num*2 for num in my_new_list}
 print(type(new_dictionary1))#dict
 print(new_dictionary1)
+
+#even odd
+new_dictionary2 = {num:("even" if num%2 ==0 else "odd" ) for num in my_new_list}
+print(new_dictionary2)
---
+++ Step2/4.SetComprehension.py
@@ -0,0 +1,6 @@
+#Set Comprehension
+num_set = {n*2 for n in range(10)}
+print(num_set)
+
+char_set = {char for char in "How are you?"}#random
+print(char_set)
