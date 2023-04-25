a2874be67956cce93db0bf3c0de809e04c01e3b3
Khine <khinezar70122107@gmail.com>
2023-04-24 16:11:29 -1200

    arbitrary arg

---
+++ Step2/18.arbitraryArg.py
@@ -0,0 +1,15 @@
+#arbitrary argument or end of positional argument
+# *
+def myFun(*arg):
+    print(arg)#return tuple
+    for d in arg:
+        print(d)
+
+myFun(4,5,6,7,8)
+
+def myFun2(a,b,c,*args):
+    print(a)
+    print(b)
+    print(c)
+    print(args)
+myFun2(40,50,60,70,80,90)
