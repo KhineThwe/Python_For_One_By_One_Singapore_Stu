d84e42b29c2bde5fa52cb60fd58082e7b19cfb86
Khine <khinezar70122107@gmail.com>
2023-04-24 16:58:00 -1200

    docString2

---
+++ Step3/2.docString1.py
@@ -0,0 +1,14 @@
+#one line docstring
+#multi-line docstring
+import pickle
+import os
+def power(a,b):
+    """Return arg1 raised to power arg2"""
+    """
+      Return arg1
+    """
+    return a**b
+print(power.__doc__)
+print(print.__doc__)
+print(pickle.__doc__)
+print(os.__doc__)
