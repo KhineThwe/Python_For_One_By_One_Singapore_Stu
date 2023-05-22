a9acade1cb4afc04732efcbfd5a375f8b40168fb
Khine <khinezar70122107@gmail.com>
2023-05-21 19:35:26 -1200

    may 21 access specifier

---
+++ OOP/accessModifier/App.py
@@ -0,0 +1,7 @@
+from Machine import *
+if __name__ == '__main__':
+    machine = Machine()
+    print(machine.id)
+    print(machine.count)
+    print(machine._color)
+    print(machine._Machine__name)#acessing private variable like this
