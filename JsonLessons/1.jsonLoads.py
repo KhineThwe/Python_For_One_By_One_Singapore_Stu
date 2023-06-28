bc3aa07f9f4fd361f7172503d4d775d75caa9142
Khine <khinezar70122107@gmail.com>
2023-06-27 15:40:51 -1200

    json

---
+++ JsonLessons/1.jsonLoads.py
@@ -0,0 +1,33 @@
+"""
+Python                 JSON
+--------------------------------
+dict                   object
+list,tuple             array
+str                    string
+int,long,float           number
+True                     true
+False                    false
+None                     null
+-------------------------------
+JSON ---> JavaScript Object Notation
+
+JSON Encoding (Serialization)
+json.dumps():Convert Python object to json string
+json.dump():Write Python Object to a file in JSON format
+
+JSON Decoding(Deserialization)
+json.loads():Convert json string to python object
+json.load():Read a json file and convert it to a Python Object
+#https://jsonlint.com/
+"""
+import json
+myJson = """
+{
+    "name" : "tech",
+    "age": 100,
+    "hobby" : ["coding","training","building IOT"]
+}
+"""
+data = json.loads(myJson)#Encoding
+print(data)
+print(type(data))
