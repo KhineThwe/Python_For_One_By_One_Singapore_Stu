7a96389cc842e2ee968cee8fc9e227d972c335f5
Khine <khinezar70122107@gmail.com>
2023-07-30 08:36:31 -1200

    where sql

---
+++ MySQLLessons/9.format.py
@@ -0,0 +1,12 @@
+import mysql.connector
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user = "root",
+    passwd = "toor",
+    database="onlinedb2"
+)
+dbCursor = db_connection.cursor()
+dbCursor.execute("Select id,name,age FROM students")
+data = dbCursor.fetchall()
+for db in data:
+    print("%d %s %d"%(db[0],db[1],db[2]))
