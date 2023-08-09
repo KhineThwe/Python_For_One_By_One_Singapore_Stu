b2902ea451b1ba5feb86c1d5c4ebcc4b82cb36d5
Khine <khinezar70122107@gmail.com>
2023-08-08 20:12:02 -1200

    modified

--- MySQLLessons/17.joinTable.py
+++ MySQLLessons/17.joinTable.py
@@ -6,18 +6,18 @@ db_connection = mysql.connector.connect(
     database="onlinedb2"
 )
 dbCursor = db_connection.cursor()
-dbCursor.execute("CREATE TABLE cost(id int primary key AUTO_INCREMENT,monthly int,totalCost int)")
-
-dataType = "INSERT INTO cost(id,monthly,totalCost) values(%s,%s,%s)"
-data = [(102,150,1500),(104,100,1200),(4,250,2500)]
-
-dbCursor.executemany(dataType,data)
-db_connection.commit()
-print("Datas are inserted!")
+# dbCursor.execute("CREATE TABLE cost(id int primary key AUTO_INCREMENT,monthly int,totalCost int)")
+#
+# dataType = "INSERT INTO cost(id,monthly,totalCost) values(%s,%s,%s)"
+# data = [(102,150,1500),(104,100,1200),(4,250,2500)]
+#
+# dbCursor.executemany(dataType,data)
+# db_connection.commit()
+# print("Datas are inserted!")
 
 dbCursor.execute("SELECT students.id,students.name,students.age,cost.id,cost.totalCost,cost.monthly FROM cost "
                  "join students on cost.id = students.id")
 data = dbCursor.fetchall()
-print(data)
-for db in dbCursor:
+print("id  name  age  CostID  TotalCost  Monthly")
+for db in data:
     print("%d %s %d %d %d %d"%(db[0],db[1],db[2],db[3],db[4],db[5]))
