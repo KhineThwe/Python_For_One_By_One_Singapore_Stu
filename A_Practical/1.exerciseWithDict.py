10760585328b2c58cd4a7c095bc9dc8aba132ecf
khinezarthwe <khinezar70122107@gmail.com>
2023-10-08 10:10:29 -1200

    exercisewithdict

---
+++ A_Practical/1.exerciseWithDict.py
@@ -0,0 +1,14 @@
+employees = [
+    {"name": "Alice", "age": 30, "department": "HR", "salary": 50000},
+    {"name": "Bob", "age": 25, "department": "Engineering", "salary": 60000},
+    {"name": "Carol", "age": 35, "department": "Finance", "salary": 70000},
+    {"name": "David", "age": 28, "department": "Engineering", "salary": 75000},
+    {"name": "Eve", "age": 24, "department": "HR", "salary": 55000}
+]
+# 1.Calculate and print the average salary of all employees.
+# 2.Find and print the name of the oldest employee.
+# 3.Create a dictionary that groups employees by their department. The keys should be department names, and the values should be lists of employees in that department.
+# 4.Calculate and print the total salary expenditure for each department.
+# 5.Identify the department with the highest total salary expenditure and print its name.
+# 6.Create a new list of dictionaries that contains the same employee information but with an additional key called "bonus". The "bonus" should be calculated as 10% of each employee's salary.
+# 7.Print the updated list of dictionaries.
